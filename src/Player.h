36a241d502cc5ad1ad81fde66f42b219e4cf9d55
jensenrrr <32421710+jensenrrr@users.noreply.github.com>
2017-11-14 20:45:51 -0500

    Update Player.h

--- src/Player.h
+++ src/Player.h
@@ -1,11 +1,18 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "Piece.h"
 class Player {
 private:
+	int score;
+	Piece ***playerPieces [2][16];
 
 public:
-	Player();
+	Player(bool isHuman);
+	int getScore();
+	void playCard(int index);
+	void setScore();
+	void printCards();
 };
 
 #endif
